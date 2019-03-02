871866273b0cd187056045ac2a6061e85bb0b5c6
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-02 14:40:17 -0300

    implementando o filtro de autenticacao

--- src/main/java/br/com/vfs/marketplacebackend/filter/JwtAuthTokenFilter.java
+++ src/main/java/br/com/vfs/marketplacebackend/filter/JwtAuthTokenFilter.java
@@ -1,4 +1,66 @@
 package br.com.vfs.marketplacebackend.filter;
 
-public class JwtAuthTokenFilter {
+import br.com.vfs.marketplacebackend.provider.JwtProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthTokenFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    private final UserDetailsService userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthTokenFilter.class);
+
+    @Autowired
+    public JwtAuthTokenFilter(JwtProvider jwtProvider, UserDetailsService userDetailsService) {
+        this.jwtProvider = jwtProvider;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String jwt = getTokenJwtFromHeader(httpServletRequest);
+            if (jwt != null && jwtProvider.validateJwtToken(jwt)) {
+                String username = jwtProvider.getUserNameFromJwtToken(jwt);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("Usuário não pode ser autenticado -> Mensagem: {}", e);
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+
+    private String getTokenJwtFromHeader(HttpServletRequest httpServletRequest) {
+        String authHeader = httpServletRequest.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
 }
