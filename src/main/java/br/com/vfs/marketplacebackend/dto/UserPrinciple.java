18026a0a9e427ea239f46eaa1f5691a32a55ec5f
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-02 14:31:39 -0300

    criando objeto userPrinciple e servico que implementa userDetailsService

---
+++ src/main/java/br/com/vfs/marketplacebackend/dto/UserPrinciple.java
@@ -0,0 +1,73 @@
+package br.com.vfs.marketplacebackend.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Objects;
+
+@Builder
+@Data
+public class UserPrinciple implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserPrinciple user = (UserPrinciple) o;
+        return Objects.equals(id, user.id);
+    }
+}
---
+++ src/main/java/br/com/vfs/marketplacebackend/service/UserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package br.com.vfs.marketplacebackend.service;
+
+import br.com.vfs.marketplacebackend.dto.UserPrinciple;
+import br.com.vfs.marketplacebackend.entity.UserEntity;
+import br.com.vfs.marketplacebackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException(String.format("Usuário não encontrado. Nickname ou email: %s", username))
+        );
+        return UserPrinciple.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .email(user.getEmail())
+                .authorities(user.getRoles().stream().map(role ->
+                        new SimpleGrantedAuthority(role.getName().name())
+                ).collect(Collectors.toList()))
+                .build();
+    }
+}
