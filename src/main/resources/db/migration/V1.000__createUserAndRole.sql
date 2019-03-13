670f3421ed2599ad5042bb0d60491eca1a8ea0f0
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-12 23:49:20 -0300

    alterações efetuadas para subir o mysql e o projeto spring em container

--- src/main/resources/db/migration/V1.000__createUserAndRole.sql
+++ src/main/resources/db/migration/V1.000__createUserAndRole.sql
@@ -7,19 +7,19 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQUE_users_username` (`username`),
   UNIQUE KEY `UNIQUE_users_email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `roles` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UNIQUE_roles_name` (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 CREATE TABLE `user_roles` (
   `user_id` bigint(20) NOT NULL,
   `role_id` bigint(20) NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
-  KEY `FK_user_roles_role_id` (`role_id`),
-  KEY `FK_user_roles_user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
+);
