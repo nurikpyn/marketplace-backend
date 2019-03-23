637b94ad654fda08421364d829ba4d3d1ea3c409
Vinicius Faria de Souza <vinicius_ufop@yahoo.com.br>
2019-03-23 13:27:43 -0300

    acerto na anotação de repositorio

--- src/main/java/br/com/vfs/marketplacebackend/repository/ProductImageRepository.java
+++ src/main/java/br/com/vfs/marketplacebackend/repository/ProductImageRepository.java
@@ -2,7 +2,9 @@ package br.com.vfs.marketplacebackend.repository;
 
 import br.com.vfs.marketplacebackend.entity.ProductImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductImageRepository extends JpaRepository<ProductImageEntity, Long> {
 
 }
